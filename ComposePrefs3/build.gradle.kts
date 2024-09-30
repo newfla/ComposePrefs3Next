06b67eaca2e32d4f50dea3a99bd0e29df6c5d74e
newfla <newfla@hotmail.it>
2024-09-30 13:03:53 +0200

    Updated gradle configs files

---
+++ ComposePrefs3/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.maven.publish)
+}
+
+android {
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+
+        testInstrumentationRunner ="androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    namespace = "com.jamal.composeprefs3"
+}
+
+dependencies {
+    implementation(libs.compose.ui)
+    implementation(libs.material3)
+    implementation(libs.material3.window.size)
+    implementation(libs.datastore.preferences)
+
+    androidTestImplementation(libs.ui.test.junit4)
+    testImplementation(libs.junit)
+}
+
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.github.jamalmulla"
+                artifactId = "ComposePrefs3"
+                version = "1.0.7"
+            }
+        }
+    }
+}
---
+++ app/build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+}
+
+android {
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.jamal.composeprefs3sample"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled  = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    namespace = "com.jamal.composeprefs3sample"
+}
+
+dependencies {
+
+    implementation (libs.core.ktx)
+    implementation (libs.material)
+    implementation (libs.compose.ui)
+    implementation (libs.material3)
+    implementation (libs.material3.window.size)
+    implementation (libs.ui.tooling.preview)
+    implementation (libs.lifecycle.runtime.ktx)
+    implementation (libs.activity.compose)
+    testImplementation (libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.ui.test.junit4)
+    debugImplementation(libs.ui.tooling)
+
+    // ComposePrefs library
+    implementation(project(":ComposePrefs3"))
+    implementation(libs.datastore.preferences)
+}
