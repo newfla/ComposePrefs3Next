c0fe0b8b6ba8d0b149f694ef03ecb7734f34831d
newfla <newfla@hotmail.it>
2024-11-14 18:21:56 +0100

    Bump compile/target sdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.jamal.composeprefs3sample"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
