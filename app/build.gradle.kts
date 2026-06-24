6f36e2ec1e2406a702fa3f3339d8d19eb19da71b
newfla <newfla@hotmail.it>
2026-06-24 10:43:59 +0200

    chore: bump SDK versions to 37 and update gradle properties
    Bumps `compileSdk` and `targetSdk` from 36 to 37.
    
    Updates `android.enableJetifier` to `false` in `gradle.properties`.
    
    Updates `android.nonFinalResIds` to `true` in `gradle.properties`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 
 android {
 
-    compileSdk = 36
+    compileSdk = 37
     defaultConfig {
         applicationId = "com.jamal.composeprefs3sample"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
